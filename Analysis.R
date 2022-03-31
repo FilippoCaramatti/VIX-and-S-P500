8ddf5500078a5be21e84b8d97a6aeca5c5789a5b
FilippoCaramatti <cinefilippo@gmail.com>
2022-03-31 15:26:35 +0200

    esempio dei tipi rifatto e venuto

--- Analysis.R
+++ Analysis.R
@@ -1,24 +1,24 @@
 library(tseries)
 library(xts)
 library(readxl)
-s_date<-"1990-01-22"
-e_date<-"2022-03-24"
+s_date<-"1990-01-02"
+e_date<-"2009-12-31"
 sp500<-as.xts(get.hist.quote("^GSPC", start = s_date, end = e_date, quote = "Close", provider = "yahoo", retclass = "zoo"))
-vix<-as.xts(get.hist.quote("^VIX", start = s_date, end = e_date, quote = "Close", provider = "yahoo", retclass = "zoo"))
 sp500_lr<-na.omit(diff(log(sp500))) 
-
 #non ho messo na.omit così si vede se il lag ha funzionato: 
   # dando per scontato che la prima sia giusta quella della volatilità futura dovrebbe avere lo stesso numero di quella passata ma 21 giorni prima a livello di data nella time series
   #dovrebbe avere gli na alla fine e non all'inizio
 sp500_sd_h22<-rollapply(sp500_lr, width = 22, FUN = sd)
-sp500_sd_f22<-lag.xts(sp500_sd_h22, k=-21)
+sp500_sd_f22<-na.omit(lag.xts(sp500_sd_h22, k=-21))*sqrt(252)*100
 
-sp500_sd_h11<-na.omit(rollapply(sp500_lr, width = 11, FUN = sd))
-sp500_sd_h22<-na.omit(rollapply(sp500_lr, width = 22, FUN = sd))
+length(sp500_sd_f22)
 
-#altre cose - prove a caso
-as.zoo(read_xlsx("sp500.xlsx", col_types = c("date", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")))
+vix<-as.xts(get.hist.quote("^VIX", start = s_date, end = e_date, quote = "Close", provider = "yahoo", retclass = "zoo"))
+vix_f22<-(vix[2:5021])
+length(vix_f22)
 
-write.zoo(sp500, file="C:\\Users\\cinef\\Documents\\GitHub\\VIX_and_SP500\\sp500.csv")
-
-%>% 
+model_f22<-lm(sp500_sd_f22~vix_f22)
+summary(model_f22)
+plot_f22<- cbind(coredata(vix_f22),(coredata(sp500_sd_f22)))
+plot_f22(plot_f22)
+abline(model_f22)
