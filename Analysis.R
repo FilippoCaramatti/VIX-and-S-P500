1535a410798043b155e42998871d40ff5e80528b
FilippoCaramatti <cinefilippo@gmail.com>
2022-04-05 14:02:07 +0200

    forward model function added

--- Analysis.R
+++ Analysis.R
@@ -6,6 +6,30 @@ library(readxl)
 s_date<-"1990-01-02"
 e_date<-"2009-12-31"
 
+f_model<-function(d) {
+  #sp500 data
+  sp500<-as.xts(get.hist.quote("^GSPC", start = s_date, end = e_date, quote = "Close", provider = "yahoo", retclass = "zoo"))
+  sp500_lr<-na.omit(diff(log(sp500))) 
+  #vix data
+  vix<-as.xts(get.hist.quote("^VIX", start = s_date, end = e_date, quote = "Close", provider = "yahoo", retclass = "zoo"))
+  # sp500 future 22 day volatility as historical volatility lagged backward of 22 days
+  sp500_sd_f<-na.omit(
+    lag.xts(
+      rollapply(sp500_lr, width = d, FUN = sd),
+      k=-(d-1)
+    )
+  )*sqrt(252)*100
+  n<-length(vix)
+  vix_f<-(vix[2:(n-(d-1))])
+  
+  model<-lm(sp500_sd_f~vix_f)
+  plott<- cbind(coredata(vix_f),(coredata(sp500_sd_f)))
+  cor<-cor(coredata(vix_f),(coredata(sp500_sd_f)))
+  model
+}
+f_model(d=22)
+
+
 #sp500 data
 sp500<-as.xts(get.hist.quote("^GSPC", start = s_date, end = e_date, quote = "Close", provider = "yahoo", retclass = "zoo"))
 sp500_lr<-na.omit(diff(log(sp500))) 
